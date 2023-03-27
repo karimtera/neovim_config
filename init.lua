b4f3174eaa8a981174be095991280fc9e9ed75cc
Karim Tera <karim.tera1@gmail.com>
2023-03-28 00:59:52 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,73 @@
+require('basics')
+require('colors')
+require('telescope-config')
+require('lsp-zero-config')
+require('lualine').setup()
+require('gitsigns').setup()
+
+require'nvim-treesitter.configs'.setup {
+  -- ensure_installed = "maintained",
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true,
+    disable = { "lua" }
+  },
+  indent = {
+    enable = true
+  }
+}
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  -- use {'neoclide/coc.nvim', branch = 'release'}
+  use 'folke/tokyonight.nvim'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    tag = "v0.7.2"
+  }
+  use 'tpope/vim-commentary'
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use {
+    'lewis6991/gitsigns.nvim',
+    tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+     requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    "ur4ltz/surround.nvim",
+    config = function()
+      require"surround".setup {mappings_style = "surround"}
+    end
+  }
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
+}
+end)
---
+++ lua/lsp-zero-config.lua
@@ -0,0 +1,28 @@
+-- local lsp = require('lsp-zero').preset({
+--   name = 'minimal',
+--   set_lsp_keymaps = true,
+--   manage_nvim_cmp = true,
+--   suggest_lsp_servers = false,
+-- })
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
+lsp.nvim_workspace()
+
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "]g", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "[g", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
+
